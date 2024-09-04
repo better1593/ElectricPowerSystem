767ff21015bc4170986dc7c681687001428c290c
xuetingshelly <xueting.zhang@campus.tu-berlin.de>
2024-09-04 17:28:52 +0800

    求解

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     network = Network()
     network.run(file_name, strategy)
     print(network.solution)
-    Y13_Tower_1 = network.solution.loc["Y13_Tower_1"].tolist()
+    Y13_Tower_1 = [abs(i)for i in network.solution.loc["Y13_Tower_1"].tolist()]
     X05 = network.solution.loc["X05"].tolist()
     X08 = network.solution.loc["X08"].tolist()
     min = [abs(a - b) for a, b in zip(X05, X08)]
 
     # 生成数据
     x = np.arange(0, 10000, 1)  # 横坐标数据为从0到10之间，步长为0.1的等差数组
-    y = min  #
+    y = Y13_Tower_1[:10000]  #
 
     # 生成图形
     plt.plot(x, y)
